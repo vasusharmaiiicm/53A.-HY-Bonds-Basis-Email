1f402dc53462e08739f7a3488b1a5d4d1bc214b4
vasusharmaiiicm <vasu.sharma@iiicm.com>
2025-08-29 16:17:55 -0400

    Add files via upload

---
+++ HY Basis.py
@@ -0,0 +1,25 @@
+import asyncio
+from nbconvert import NotebookExporter
+from nbconvert.preprocessors import ExecutePreprocessor
+from nbformat import read, write
+
+print("setting policy")
+asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+input_ntbk = "J:\\53. Live HY Basis\\v67.ipynb"
+output_ntbk = "J:\\53. Live HY Basis\\v67.ipynb"
+
+print("loading ntbk")
+
+with open(input_ntbk, "r", encoding="utf-8") as f:
+	notebook = read(f, as_version=4)
+
+print("executing ntbk")
+executor = ExecutePreprocessor(timeout=60000, kernel_name="python3")
+executor.preprocess(notebook, {'metadata': {'path': "J:\\53. Live HY Basis\\"}})
+
+print("saving ntbk")
+with open(output_ntbk, "w", encoding="utf-8") as f:
+	write(notebook,f)
+
+print("Done")
