c1558b49589c6e471d808a804e38930d05d437b9
vasusharmaiiicm <vasu.sharma@iiicm.com>
2025-09-02 18:05:48 -0400

    Add files via upload

--- HY Basis.py
+++ HY Basis.py
@@ -6,8 +6,8 @@ from nbformat import read, write
 print("setting policy")
 asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
-input_ntbk = "J:\\53. Live HY Basis\\v3.ipynb"
-output_ntbk = "J:\\53. Live HY Basis\\v3.ipynb"
+input_ntbk = "J:\\53. Live HY Basis\\v11.ipynb"
+output_ntbk = "J:\\53. Live HY Basis\\v11.ipynb"
 
 print("loading ntbk")
 
